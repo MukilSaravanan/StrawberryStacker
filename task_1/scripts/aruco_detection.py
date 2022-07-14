dcce55806c44ca0fa4ec03a86c143b8b86ae098c
sanjaykumar30 <m.sanjaykumar3072002@gamil.com>
2022-07-14 22:37:59 +0530

    update task_1

--- task_1/scripts/aruco_detection.py
+++ task_1/scripts/aruco_detection.py
@@ -6,12 +6,12 @@
 
 import numpy as np
 import cv2
-from cv2 import aruco
+import cv2.aruco as aruco
 import time
 from aruco_library import *
 
 
-image_list = ["../Test_images/Test_image1.png","../Test_images/Test_image2.png"]
+image_list = ["../scripts/test_image1.png", "../scripts/test_image2.png"]
 test_num = 1
 
 for image in image_list:
@@ -19,7 +19,6 @@ for image in image_list:
 	Detected_ArUco_markers = detect_ArUco(img)									## detecting ArUco ids and returning ArUco dictionary
 	angle = Calculate_orientation_in_degree(Detected_ArUco_markers)				## finding orientation of aruco with respective to the menitoned scale in problem statement
 	img = mark_ArUco(img,Detected_ArUco_markers,angle)						## marking the parameters of aruco which are mentioned in the problem statement
-	result_image = "../Test_images/Result_image"+str(test_num)+".png"
+	result_image = "../scripts/Result_image"+ str(test_num)+".png"
 	cv2.imwrite(result_image,img)									## saving the result image
 	test_num = test_num +1
-
